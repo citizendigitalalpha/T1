4035a7e1597cd8a280b2ed7dc19bce574f43c0f7
chrishewison1983 <ods.chrishewison@gmail.com>
2016-11-30 08:30:55 +0000

    Design changes and explore new design

--- app/assets/javascripts/dynamic-panels.js
+++ app/assets/javascripts/dynamic-panels.js
@@ -5,67 +5,37 @@ function getValues() {
      var vals = $selects.map(function() {
           return this.value;
      }).get();
-     if (vals.join('') === "nurse" + "one") {
-          
-          $(".dynamic-panels").addClass("visible");
-          $("#nurse-one").fadeIn("slow");
-          $("#nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-     } else if (vals.join('') === "nurse" + "two") {
-          
-          $(".dynamic-panels").addClass("visible");
-          $("#nurse-two").fadeIn("slow");
-          $("#nurse-one, #nurse-three, #nurse-four, #nurse-five").hide();
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-     } else if (vals.join('') === "nurse" + "three") {
-          
-          $(".dynamic-panels").addClass("visible");
-          $("#nurse-three").fadeIn("slow");
-          $("#nurse-one, #nurse-two, #nurse-four, #nurse-five").hide();
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-     } else if (vals.join('') === "nurse" + "four") {
-          
-          $(".dynamic-panels").addClass("visible");
-          $("#nurse-four").fadeIn("slow");
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-five").hide();
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-     } else if (vals.join('') === "nurse" + "five") {
-          
-          $(".dynamic-panels").addClass("visible");
-          $("#nurse-five").fadeIn("slow");
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four").hide();
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-     } else if (vals.join('') === "paralegal" + "one") {
-          
-          $(".dynamic-panels").addClass("visible");
+     if (vals.join('') === "one") {
+          // $(".dynamic-panels").addClass("visible");
           $("#paralegal-one, #paralegal-two, #paralegal-three").fadeIn("slow");
-          $("#paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-     } else if (vals.join('') === "paralegal" + "two") {
-          
-          $(".dynamic-panels").addClass("visible");
+          $(".intro-yes.one").fadeIn("slow");
+          $("#paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine, .intro").hide();
+          $(".intro-yes.two, .intro-yes.three, .intro-yes.four, .intro-yes.five").hide();
+     } else if (vals.join('') === "two") {
+          // $(".dynamic-panels").addClass("visible");
           $("#paralegal-four, #paralegal-five").fadeIn("slow");
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-     } else if (vals.join('') === "paralegal" + "three") {
-          
-          $(".dynamic-panels").addClass("visible");
+          $(".intro-yes.two").fadeIn("slow");
+          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-six, #paralegal-seven, #paralegal-eight, #paralegal-nine, .intro").hide();
+          $(".intro-yes.one, .intro-yes.three, .intro-yes.four, .intro-yes.five").hide();
+     } else if (vals.join('') === "three") {
+          // $(".dynamic-panels").addClass("visible");
           $("#paralegal-six").fadeIn("slow");
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-seven, #paralegal-eight, #paralegal-nine").hide();
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-     } else if (vals.join('') === "paralegal" + "four") {
-          
-          $(".dynamic-panels").addClass("visible");
+          $(".intro-yes.three").fadeIn("slow");
+          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-seven, #paralegal-eight, #paralegal-nine, .intro").hide();
+          $(".intro-yes.one, .intro-yes.two, .intro-yes.four, .intro-yes.five").hide();
+     } else if (vals.join('') === "four") {
+          // $(".dynamic-panels").addClass("visible");
           $("#paralegal-seven, #paralegal-eight").fadeIn("slow");
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-nine").hide();
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-     } else if (vals.join('') === "paralegal" + "five") {
-          
-          $(".dynamic-panels").addClass("visible");
+          $(".intro-yes.four").fadeIn("slow");
+          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-nine, .intro").hide();
+          $(".intro-yes.one, .intro-yes.two, .intro-yes.three, .intro-yes.five").hide();
+     } else if (vals.join('') === "five") {
+          // $(".dynamic-panels").addClass("visible");
           $("#paralegal-nine").fadeIn("slow");
-          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight").hide();
-          $("#nurse-one, #nurse-two, #nurse-three, #nurse-four, #nurse-five").hide();
-     } else {          
+          $(".intro-yes.five").fadeIn("slow");
+          $("#paralegal-one, #paralegal-two, #paralegal-three, #paralegal-four, #paralegal-five, #paralegal-six, #paralegal-seven, #paralegal-eight, .intro").hide();
+          $(".intro-yes.one, .intro-yes.two, .intro-yes.three, .intro-yes.four").hide();
+     } else {
           $(".dynamic-panels").removeClass("visible");
      }
 }
